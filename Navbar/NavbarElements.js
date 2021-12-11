865177b24bbae95167f66e233a08365052cddedb
Kenny Zhao <79938837+kennyzhao-code@users.noreply.github.com>
2021-12-10 23:57:16 -0500

    new navbar

---
+++ Navbar/NavbarElements.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+
+export const Nav = styled.nav`
+    height: 0.1px; 
+    display: flex; 
+    font-size: 1rem; 
+    position: sticky; 
+    top: 0; 
+    z-index: 10;
+
+    @media screen and (max-width: 960px)
+    {
+        transition: 0.8s all ease; 
+    }
+`
+
+export const NavbarContainer = styled.div`
+    display: flex; 
+    justify-content: space-between; 
+    height: 80px;
+    z-index: 1;
+    width: 100%
+    padding: 0 24px; 
+    max-width: 1100px;
+ 
+`
+
+export const FullIcon = styled.div`
+        display: block;
+        top: 1rem; 
+        font-size: 1.8rem; 
+        cursor: pointer; 
+        color: white; 
+        margin-left: 31.5px;
+        position: relative; 
+        color: black;
+        opacity: 0.3;
+    
+        &:hover {
+            opacity: 1;
+        }
+
+        @media screen and (max-width: 768px){
+            display: none;
+        }
+`
+
+export const MobileIcon = styled.div`
+        display: none;
+
+        @media screen and (max-width: 768px){
+            display: block;
+            top: 1rem; 
+            font-size: 1.5rem; 
+            cursor: pointer; 
+            color: white; 
+            position: relative; 
+            color: black;
+            opacity: 0.3;
+            margin-left: 31.5px;
+        }
+
+        &:hover{
+            opacity: 1;
+        }
+`
+
+
+
---
+++ Navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {IoIosArrowDown} from 'react-icons/io'
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer,  FullIcon, MobileIcon} from "./NavbarElements"
+
+const Navbar = ({ toggle, toggleMobile }) => {
+    return (
+        <Nav>
+            <NavbarContainer>
+
+                <FullIcon onClick={toggle}>
+                    <IoIosArrowDown />
+                </FullIcon>
+
+                <MobileIcon onClick={toggleMobile}>
+                    <FaBars />
+                </MobileIcon>
+
+            </NavbarContainer>
+        </Nav>
+     )
+}
+
+export default Navbar
