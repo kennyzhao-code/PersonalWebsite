fca7954974e868848b0967bbc7bea939fba6960b
Kenny Zhao <kennyzhao2004@gmail.com>
2023-02-11 01:30:16 -0500

    font update

--- src/components/Fonts/fontStyles.js
+++ src/components/Fonts/fontStyles.js
@@ -1,8 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 import OktaNeueBold1 from '../Fonts/OktaNeue-BlackItalic.woff';
 import OktaNeueBold2 from '../Fonts/OktaNeue-BlackItalic.woff2';
+import OktaNeueBold3 from '../Fonts/OktaNeue-BlackItalic.eot';
+import OktaNeueBold4 from '../Fonts/OktaNeue-BlackItalic.ttf';
+import OktaNeueBold5 from '../Fonts/OktaNeue-BlackItalic.svg';
+
+
 import OktaNeueLight1 from '../Fonts/OktaNeue-UltraLight.woff';
 import OktaNeueLight2 from '../Fonts/OktaNeue-UltraLight.woff2';
+import OktaNeueLight3 from '../Fonts/OktaNeue-UltraLight.eot';
+import OktaNeueLight4 from '../Fonts/OktaNeue-UltraLight.ttf';
+import OktaNeueLight5 from '../Fonts/OktaNeue-UltraLight.svg';
 
 
 const FontStyles = createGlobalStyle`
@@ -10,12 +18,20 @@ const FontStyles = createGlobalStyle`
         font-family: 'OktaNeueBold';
         src: url(${OktaNeueBold1}) format('woff');
         src: url(${OktaNeueBold2}) format('woff2');
+        src: url(${OktaNeueBold3}) format('eot');
+        src: url(${OktaNeueBold4}) format('truetype');
+        src: url(${OktaNeueBold5}) format('svg');
+
     }
 
     @font-face {
         font-family: 'OktaNeueLight';
         src: url(${OktaNeueLight1}) format('woff');
         src: url(${OktaNeueLight2}) format('woff2');
+        src: url(${OktaNeueLight3}) format('eot');
+        src: url(${OktaNeueLight4}) format('truetype');
+        src: url(${OktaNeueLight5}) format('svg');
+
     }
 
 `
