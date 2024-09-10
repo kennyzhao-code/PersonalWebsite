22889f4fd0f7d8cd7aa4ba5ef23c9cdb5d18dd3e
Kenny Zhao <kennyzhao2004@gmail.com>
2024-09-09 22:31:47 -0400

    removed gatsby offline for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       },
     },
 
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ public/646e0218-dbcb555b7204cd54335d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[535],{5399:function(t,a,e){e.d(a,{ATZ:function(){return n}});var r=e(2225);function n(t){return(0,r.k5)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24"},child:[{tag:"path",attr:{strokeWidth:"2",d:"M1 22V9.76a2 2 0 01.851-1.636l9.575-6.72a1 1 0 011.149 0l9.574 6.72A2 2 0 0123 9.76V22a1 1 0 01-1 1h-5.333a1 1 0 01-1-1v-5.674a1 1 0 00-1-1H9.333a1 1 0 00-1 1V22a1 1 0 01-1 1H2a1 1 0 01-1-1z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=646e0218-dbcb555b7204cd54335d.js.map
